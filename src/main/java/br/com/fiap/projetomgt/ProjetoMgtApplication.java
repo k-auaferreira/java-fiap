398f7dceac1b01cfb990b72611024c90bcd8b678
k-auaferreira <kaua.fs@outlook.com>
2025-08-22 23:09:58 -0300

    :tada: init: first commit

---
+++ src/main/java/br/com/fiap/projetomgt/ProjetoMgtApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.projetomgt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoMgtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetoMgtApplication.class, args);
+    }
+
+}
