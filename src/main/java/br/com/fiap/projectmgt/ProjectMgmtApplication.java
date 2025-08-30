45a631907418a954c0a0558845ff2a352c437ed8
k-auaferreira <kaua.fs@outlook.com>
2025-08-29 21:09:36 -0300

    :art: refactor: rename application class for consistency

--- src/main/java/br/com/fiap/projectmgt/ProjectMgmtApplication.java
+++ src/main/java/br/com/fiap/projectmgt/ProjectMgmtApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProjetoMgtApplication {
+public class ProjectMgmtApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ProjetoMgtApplication.class, args);
+        SpringApplication.run(ProjectMgmtApplication.class, args);
     }
 
 }
